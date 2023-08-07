e5beac5943b4c8c53ae75cb404632273856908ff
adityach0uhan <m4with6x@gmail.com>
2023-08-07 23:37:52 +0530

    background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import {  useState } from 'react';
 import './App.css';
 function App() {
+
+
+  const [background, setBackground] = useState("")
+
+  let weatherMood = "";
+  let checkWeather = "rainy";
+  switch (checkWeather) {
+
+    case "rainy":
+      return weatherMood="/images/rainy.jpg"
+      break;
+    
+    case "sunny":
+      return weatherMood = "/images/sunny.jpg";
+      break;
+
+  }
+  
+
+  setBackground("/images/cloudy.jpg")
+  
+
   return (
     <>
 
-      <h4>Weather Web App</h4>
+      <div className='Main-Container' style={{
+        background: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        minWidth:'100vw',
+        minHeight: '100vh'
+        }} >
+
+
+
+
+
+      </div>
       
     </>
   );

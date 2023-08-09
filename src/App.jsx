f4929b5c79ac2e150ecb485c9cffbc94c9a60533
adityach0uhan <m4with6x@gmail.com>
2023-08-09 14:20:20 +0530

    API fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,45 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 function App() {
-  
-  const [background, setBackground] = useState("");
-  const [weatherData, setWeatherData] = useState([])
 
-  function getWeatherData() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(response => response.json())
-      .then(data => setWeatherData(data))
-      .catch(error => console.log(error))
-  }
+  const API_KEY = process.env.REACT_APP_API
 
 
   let weatherMood = "sunny";
-
   let city = "pune"
 
+  
+  const [background, setBackground] = useState("");
+  // const [weatherData, setWeatherData] = useState([])
+  useEffect(() => { setBackground(`/images/${weatherMood}.jpg`) }, [background]);
+  
+  
+  async function getWeatherData() {
+    
+    try {
+      const fetchData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+      const APIdata=await fetchData.json()
+      
+      const { temp, feels_like, humidity } = APIdata.main 
+      const windSpeed = APIdata.wind.speed;
+      const location = APIdata.name;
 
-  useEffect(() => { setBackground(`/images/${weatherMood}.jpg`); getWeatherData() }, [background]);
+      const { country, sunrise, sunset } = APIdata.sys;
+      console.log(country)
+      
+      
+    } catch(e) {
+      console.log(e)
+    }
+  }
+  
+  
+  useEffect(() => { getWeatherData() }, [])
+
+ 
+
+
+
 
 
 
@@ -34,7 +55,7 @@ function App() {
 
         <img className='background-img' src={background} alt="" />
 
-        {console.log(weatherData.temp)}
+        {/* {console.log(weatherData.weather[0].main)} */}
 
 
 
